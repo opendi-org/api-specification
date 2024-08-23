6b69a91dd7b79fb6571e44445fd777686c4dcce5
Actions: Generate MKDocs Website <actions-generate-mkdocs-website@users.noreply.github.com>
2024-08-23 16:53:26 +0000

    Action: Move pages src to staging dir

---
+++ docs/assets/js/452.8dda98c8.js
@@ -0,0 +1,14 @@
+"use strict";
+exports.id = 452;
+exports.ids = [452];
+exports.modules = {
+
+/***/ 452:
+/***/ ((module) => {
+
+module.exports = /*#__PURE__*/JSON.parse('{"$schema":"http://json-schema.org/draft-07/schema#","$id":"DI-Asset-v0.0-alpha-0824","title":"UUID","$comment":"Ensures UUID validation is consistent.","type":"string","format":"uuid","pattern":"^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$"}');
+
+/***/ })
+
+};
+;
